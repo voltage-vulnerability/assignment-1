911f1bb5d250ec8e3411f8b627ea177b6dc177f7
voltage-vulnerability <75689221+voltage-vulnerability@users.noreply.github.com>
2026-06-13 09:53:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # 1. HARDCODED SECRET VULNERABILITY1
 # CodeQL / Secret Scanning will flag this as an exposed credential risk.
-API_SECRET_KEY = ${{secrets.API_SECRET_KEY}}
+API_SECRET_KEY:{{secrets.API_SECRET_KEY}}
 
 @app.route("/profile")
 def user_profile():
